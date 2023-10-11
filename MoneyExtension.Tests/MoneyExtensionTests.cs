1cc2466e1f08fed8331ceaf65309a8f8c0909257
Wellington Andrade <wellington.andrade@hotmail.com.br>
2023-10-11 14:55:52 -0300

    Primeiro commit

---
+++ MoneyExtension.Tests/MoneyExtensionTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MoneyExtension.Tests
+{
+    [TestClass]
+    public class MoneyExtensionTests
+    {
+        [TestMethod]
+        public void DeveConverterDecimalParaIntenrio()
+        {
+            decimal valor = 279.98M;
+            var cents = valor.ToCents();
+
+            Assert.AreEqual(27898, cents);
+        }
+    }
+}
---
+++ MoneyExtension/MoneyExtension.cs
@@ -0,0 +1,26 @@
+﻿namespace MoneyExtension
+{
+    public static class MoneyExtension
+    {
+        public static int ToCents(this decimal amount)
+        {
+            if (amount <= 0)
+            {
+                return 0;
+            }
+
+            var text = amount.ToString("N2")
+                .Replace(",", "")
+                .Replace(".", "");
+
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return 0;
+            }
+
+            int.TryParse(text, out var result);
+            return result;
+        }
+
+    }
+}
